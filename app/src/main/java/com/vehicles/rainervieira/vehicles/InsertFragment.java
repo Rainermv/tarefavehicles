c23497a8420bed50087fca71f4ba4a95ae585f83
Rainer Vieira <rainermv@gmail.com>
2016-07-05 10:25:38 -0300

    modelo base para paginação

---
+++ app/src/main/java/com/vehicles/rainervieira/vehicles/InsertFragment.java
@@ -0,0 +1,22 @@
+package com.vehicles.rainervieira.vehicles;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Rainer on 05/07/2016.
+ */
+public class InsertFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_insert, container, false);
+
+        return rootView;
+    }
+}
